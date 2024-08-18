4b22390553667172ad7d26cfae5291059a0b5742
trujillo <alex55trujillo@gmail.com>
2024-08-18 15:37:35 -0600

    Cambios contacto

--- src/Screens/Contacto.js
+++ src/Screens/Contacto.js
@@ -5,19 +5,18 @@ import logo from "../Assets/logo.png";
 const Contacto = () => {
   return (
     <div className={styles.contacto}>
-      <div className={styles.title}>Contacto</div>
+      <div className={styles.title}>Información de contacto</div>
       <div className={styles.contenedor}>
         <div className={styles.contizq}>
-          <p>
-            ¿Deseas algún producto <br /> en particular?
-          </p>
+          ¿Deseas algún producto en particular o alguna asistencia
+          especializada?
         </div>
         <div className={styles.cel}>
           <p>Celular: 55-85-95-19-34</p>
         </div>
       </div>
       <div className={styles.logo}>
-        <img src={logo} alt="Logo" />
+        <img className={styles.logo} src={logo} alt="Logo" />
       </div>
     </div>
   );
